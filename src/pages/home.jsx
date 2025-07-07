a8aeba5fcb6d548723480fdda09070e489ac1baf
kuhora <kuhora123@gmail.com>
2025-07-07 14:39:37 -0300

    config css

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,9 @@
 export const Home = () => {
-    return <div>HomePage</div>
+    return <div className="minHeightScreen bg-background text-foreground overflow-x-hidden">
+        {/*theme toggle*/}
+        {/*bg effects*/}
+        {/*nav bar*/}
+        {/*main content*/}
+        {/*footer*/}
+    </div>
 }
