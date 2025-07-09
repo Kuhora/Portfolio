5edb2b03886a38c9320b03f17e08498c7be90618
kuhora <kuhora123@gmail.com>
2025-07-09 16:32:05 -0300

    Finished 2.0

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,26 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/", 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+    assetsInlineLimit: 4096,
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name].[ext]",
+        entryFileNames: "assets/[name].js",
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    strictPort: true
+  }
 });
